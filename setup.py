214127c7edadfa1b9134b73786352b3c9f504b85
prateek3211 <prateek.3211@gmail.com>
2019-02-23 13:54:03 +0530

    updated

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ This is a temporary script file.
 
 from distutils.core import setup
 setup(
-  name = 'nsepy',
+  name = 'nsepy_v1',
   packages = ['nsepy', 'nsepy.derivatives', 'nsepy.indices', 'nsepy.debt'], # this must be the same as the name above
-  version = '0.7',
+  version = '0.1',
   description = 'Library to download financial data in pandas dataframe',
   author = 'Swapnil Jariwala',
   author_email = 'sjerry4u@gmail.com',
-  url = 'https://github.com/swapniljariwala/nsepy', # use the URL to the github repo
+  url = 'https://github.com/prateek3211/nsepy', # use the URL to the github repo
   entry_points='''
     [console_scripts]
     nsecli=nsepy.cli:cli
   ''',
-  download_url = 'https://github.com/swapniljariwala/nsepy/archive/v0.7.zip', 
+  download_url = 'https://github.com/prateek3211/nsepy/archive/v0.1.zip', 
   install_requires = ['beautifulsoup4', 'requests', 'numpy', 'pandas', 'six', 'click', 'lxml'],
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
